8162d7244e3b141a4df32c8bc6e4f4a8f2702a6a
Sylus Abel <119014867+divin3circle@users.noreply.github.com>
2023-03-20 18:35:46 +0300

    react-basics

--- src/index.js
+++ src/index.js
@@ -2,44 +2,82 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./Index.css";
 
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/61SKca8LJLL._AC_UL320_.jpg",
+  author: "Sylus Abel",
+  title: "Nuclear Effect",
+};
+
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/61SKca8LJLL._AC_UL320_.jpg",
+  author: "Sam Kerr",
+  title: "Nuclear Effect 2",
+};
+
+const thirdBook = {
+  img: "https://m.media-amazon.com/images/I/61SKca8LJLL._AC_UL320_.jpg",
+  author: "Francis Dulp",
+  title: "Nuclear Effect-Final chapter",
+};
+
+const fourthBoook = {
+  img: "https://m.media-amazon.com/images/I/61SKca8LJLL._AC_UL320_.jpg",
+  author: "Vinncent Odhis",
+  title: "Nuclear Effect-Sub",
+};
+
 function BookList() {
   return (
     <section className="book-list">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        author={firstBook.author}
+        title={firstBook.title}
+        price="3000"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam optio
+        aut fugiat pariatur maiores esse nisi exercitationem animi asperiores
+        blanditiis cumque nobis dignissimos, dolor deserunt est veritatis saepe,
+        sint provident.
+      </Book>
+      <Book
+        img={secondBook.img}
+        author={secondBook.author}
+        title={secondBook.title}
+        price="3060"
+      />
+      <Book
+        img={thirdBook.img}
+        author={thirdBook.author}
+        title={thirdBook.title}
+        price="854"
+      >
+        This is the final chapter of Nuclear Effect series.Francis is an equiped
+        nuclear scientist and uses his knowledge to summarize Sylus' and Kerr's
+        works.
+      </Book>
+      <Book
+        img={fourthBoook.img}
+        author={fourthBoook.author}
+        title={fourthBoook.title}
+        price="FREE"
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = ({ img, title, author, price, children }) => {
+  /* const { img, title, author, price } = props; */
+  /*   console.log(props); */
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt="nuclear effect" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      <h4>KES {price}</h4>
+      {children}
     </article>
   );
 };
 
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/61SKca8LJLL._AC_UL320_.jpg"
-    alt="nuclear effect"
-  />
-);
-
-const Title = () => <h1>Nuclear Effect</h1>;
-const Author = () => <h4>Albert Franscis</h4>;
 ReactDOM.render(<BookList />, document.getElementById("root"));
